9fda782960afa8b1bdd099c337b6479423e69383
Ranjith Jayadevan <ranjithj@proton.me>
2026-09-01 08:55:49 +0530

    Bump version to 0.94.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.mj.yata"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.94.4"
+        versionCode = 26
+        versionName = "0.94.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
